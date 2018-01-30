46c42e696a9a442c12f7b66582b23f590667e243
sissi <xingbiyanshu@gmail.com>
2018-01-30 21:58:36 +0800

    CREATE

---
+++ app/src/main/java/com/example/sissi/database_test/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sissi.database_test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
