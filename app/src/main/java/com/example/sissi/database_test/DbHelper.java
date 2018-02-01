2f34ac38508e9e29528aae880a9c1b5888dfec99
xingbiyanshu <xingbiyanshu@gmail.com>
2018-02-01 18:45:15 +0800

    create table and insert record

--- app/src/main/java/com/example/sissi/database_test/DbHelper.java
+++ app/src/main/java/com/example/sissi/database_test/DbHelper.java
@@ -4,20 +4,23 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.List;
+
 /**
  * Created by Sissi on 2/1/2018.
  */
 
 public class DbHelper extends SQLiteOpenHelper {
-    private static final String DB_NAME = "test_db";
-    private static final int DB_VERSION = 1;
 
-    public DbHelper(Context context){
-        super(context, DB_NAME, null, DB_VERSION);
+    private List<String> sqls;
+
+    public DbHelper(Context context, String name, int version, List<String> sqls){
+        this(context, name, null, version, sqls);
     }
 
-    public DbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version){
+    public DbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version, List<String> sqls){
         super(context, name, factory, version);
+        this.sqls = sqls;
     }
 
     /* 创建Helper对象时并不会触发下面任何一个回调（实际只是做了一些赋值操作），当通过Helper对象获取数据库对象时才会触发如下回调。
@@ -32,6 +35,32 @@ public class DbHelper extends SQLiteOpenHelper {
     }
 
     @Override
+    public void onCreate(SQLiteDatabase db) {// 数据库创建
+        PcTrace.p("-o->");
+        //　创建表的合适时机是在数据库创建时，所以下面创建表．　
+        for (String sql : sqls){
+            db.execSQL(sql); // 类似SQLiteDatabase的其他接口如query，execSQL也支持占位符'?',然而在创建表时占位符的使用似乎受限，在check内和char(?)内使用均报错。
+        }
+//        String sql = "create table if not exists employee(" +
+//                "id integer primary key autoincrement," +
+//                "name text not null," +
+//                "gender char(6) default 'male' check(gender='male' or gender='female'),"+
+//                "birth int not null check(1960<=birth and birth<=2000)," +
+//                "nativePlace char(128) not null," +
+//                "address char(128) not null," +
+//                "phone char(11) not null unique," +
+//                "email char(32) unique," +
+//                "departmentId int not null check(0<departmentId));"
+//        ; // TODO 以一种更便捷的方式创建表　
+//        db.execSQL(sql); // 类似SQLiteDatabase的其他接口如query，execSQL也支持占位符'?',然而在创建表时占位符的使用似乎受限，在check内和char(?)内使用均报错。
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        PcTrace.p("-o-> oldVersion=%s, newVersion=%s", oldVersion, newVersion);
+    }
+
+    @Override
     public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         PcTrace.p("-o-> oldVersion=%s, newVersion=%s", oldVersion, newVersion);
     }
@@ -42,25 +71,4 @@ public class DbHelper extends SQLiteOpenHelper {
         PcTrace.p("-o->");
     }
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        PcTrace.p("-o->");
-        String sql = "create table if not exists employee(" +
-                "id integer primary key autoincrement," +
-                "name text not null," +
-                "gender char(6) default 'male' check(gender='male' or gender='female'),"+
-                "birth int not null check(1960<=birth and birth<=2000)," +
-                "nativePlace char(128) not null," +
-                "address char(128) not null," +
-                "phone char(11) not null unique," +
-                "email char(32) unique," +
-                "departmentId int not null check(0<departmentId));"
-        ;
-        db.execSQL(sql); // 类似SQLiteDatabase的其他接口如query，execSQL也支持占位符'?',然而在创建表时占位符的使用似乎受限，在check内和char(?)内使用均报错。
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        PcTrace.p("-o-> oldVersion=%s, newVersion=%s", oldVersion, newVersion);
-    }
 }
--- app/src/main/java/com/example/sissi/database_test/MainActivity.java
+++ app/src/main/java/com/example/sissi/database_test/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.sissi.database_test;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -11,11 +16,58 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SQLiteDatabase db = new DbHelper(this).getWritableDatabase();
-//        db.query();
-//        DbHelper dbHelper = new DbHelper(this);
+
+        dbOp();
     }
 
+    private String DB_NAME = "test_db";
+    private int VERSION = 1;
+    private String createEmployeeTableSql =
+            "create table if not exists employee(" +
+                    "id integer primary key autoincrement," +
+                    "name text not null," +
+                    "gender char(6) default 'male' check(gender='male' or gender='female'),"+
+                    "birth int not null check(1960<=birth and birth<=2000)," +
+                    "nativePlace char(128) not null," +
+                    "address char(128) not null," +
+                    "phone char(11) not null unique," +
+                    "email char(32) unique," +
+                    "departmentId int not null check(0<departmentId)" +
+                    ");"
+    ; // TODO 以一种更便捷的方式创建表　// 这个放在DbHelper中还是其上层有待考量。若按onUpgrade需要见到版本号来看应该放在DbHelper中，但SQLiteDatabase对象需要表名，它应该和DbHelper处在同一层次。
 
+    private void dbOp(){
+        List<String> sqls = new ArrayList<>();
+        sqls.add(createEmployeeTableSql);
+        DbHelper dbHelper = new DbHelper(this, DB_NAME, VERSION, sqls);
+
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", "gf");
+        contentValues.put("birth", 1960);
+        contentValues.put("nativePlace", "changde");
+        contentValues.put("address", "songjiang");
+        contentValues.put("phone", "12345678901");
+        contentValues.put("email", "gf@gmail.com");
+        contentValues.put("departmentId", 1);
+
+
+        // 插入记录
+        // 法一
+        db.insert("employee", null, contentValues);
+        // 法二
+
+        // 法一
+//        db.query();
+        // 法二
+//        Cursor cursor = db.rawQuery("select * from employee", null);
+//        cursor.get;
+//        while (cursor.){
+//
+//        }
+
+        dbHelper.close();
+    }
 
 }
