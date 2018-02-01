1c9a681c9cdae921a2d6001ce51bfdf297113545
xingbiyanshu <xingbiyanshu@gmail.com>
2018-02-01 16:44:27 +0800

    create database success

--- app/src/main/java/com/example/sissi/database_test/MainActivity.java
+++ app/src/main/java/com/example/sissi/database_test/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.sissi.database_test;
 
 import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -9,5 +11,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SQLiteDatabase db = new DbHelper(this).getWritableDatabase();
+//        db.query();
+//        DbHelper dbHelper = new DbHelper(this);
     }
+
+
+
 }
