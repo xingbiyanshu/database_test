ff14422cfe866cfc81384090e8bf7b8f954e9400
xingbiyanshu <xingbiyanshu@gmail.com>
2018-02-24 16:46:19 +0800

    add DbUtils.count method

--- app/src/main/java/com/example/sissi/database_test/DbUtils.java
+++ app/src/main/java/com/example/sissi/database_test/DbUtils.java
@@ -1,7 +1,6 @@
 package com.example.sissi.database_test;
 
 import android.content.ContentValues;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDoneException;
 import android.database.sqlite.SQLiteStatement;
@@ -13,17 +12,25 @@ import android.database.sqlite.SQLiteStatement;
 public class DbUtils {
 
     /**
-     * 判断是否存在某条记录。
+     * 若whereClause不为null则判断是否存在指定表中的指定记录；
+     * 若whereClause为null则判断是否存在指定的表。
      * 该方法较通过“ query查询，然后使用cursor判断是否存在记录” 的方式高效很多，尤其在where子句中使用了like关键字的情形下。
      * */
     public static boolean exists(SQLiteDatabase db, String table, String whereClause, String[] whereArgs){
-        String sql = "select 1 from " + table + " where "+whereClause + " limit 1";
+        String sql;
+        if (null != whereClause){
+            sql = "select 1 from " + table + " where "+whereClause + " limit 1";
+        }else{
+            sql = "select 1 from " + table;
+        }
 
         SQLiteStatement sqLiteStatement = db.compileStatement(sql);
 
-        sqLiteStatement.bindAllArgsAsStrings(whereArgs);
+        if (null != whereClause && null != whereArgs) {
+            sqLiteStatement.bindAllArgsAsStrings(whereArgs);
+        }
         try {
-            long res = sqLiteStatement.simpleQueryForLong();
+            long res = sqLiteStatement.simpleQueryForLong(); // 使用SQLiteStatement能得到select语句直接返回的值，此处即为1。
             return 1==res;
         }catch (SQLiteDoneException e){
             return false;
@@ -50,6 +57,28 @@ public class DbUtils {
     }
 
     /**
+     * 计算table中满足条件的记录数量。若whereClause为null则计算所有记录数量。
+     * */
+    public static long count(SQLiteDatabase db, String table, String whereClause, String[] whereArgs){
+        String sql;
+        if (null != whereClause){
+            sql = "select count() from " + table + " where "+whereClause;
+        }else{
+            sql = "select count() from " + table;
+        }
+        SQLiteStatement sqLiteStatement = db.compileStatement(sql);
+
+        if (null != whereClause && null != whereArgs) {
+            sqLiteStatement.bindAllArgsAsStrings(whereArgs);
+        }
+        try {
+            return sqLiteStatement.simpleQueryForLong();
+        }catch (SQLiteDoneException e){
+            return 0;
+        }
+    }
+
+    /**
      * 若存在则update,若不存在则insert
      * 此方法总是会先查询表中是否已存在记录.若表规模较大则耗时较多.
      * 若允许, 可以考虑先全部删除老记录再insert所有的新纪录(开启事务)的方式以提升效率.
@@ -63,5 +92,6 @@ public class DbUtils {
             db.insert(table, null, contentValues);
         }
     }
+
 
 }
